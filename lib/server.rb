2eb40b885fef2514e07a4b9ea4c19b48137e6ad6
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-08-18 15:25:29 +0300

    Add slahs command

--- lib/server.rb
+++ lib/server.rb
@@ -2,42 +2,46 @@
 
 require 'sinatra/base'
 require 'net/http'
+require_relative './slack_authorizer.rb'
+
 # This is a class
 class API < Sinatra::Base
   # Send response of message to response_url
-  def self.send_response(response_url, msg)
-    url = URI.parse(response_url)
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    request = Net::HTTP::Post.new(url)
-    request['content-type'] = 'application/json'
-    request.body = JSON[msg]
-    http.request(request)
-  end
+  # def self.send_response(response_url, msg)
+  #   url = URI.parse(response_url)
+  #   http = Net::HTTP.new(url.host, url.port)
+  #   http.use_ssl = true
+  #   request = Net::HTTP::Post.new(url)
+  #   request['content-type'] = 'application/json'
+  #   request.body = JSON[msg]
+  #   http.request(request)
+  # end
 
-  post '/slack/events' do
-    request_data = JSON.parse(request.body.read)
-    case request_data['type']
-    when 'url_verification'
-      # URL Verification event with challenge parameter
-      request_data['challenge']
-    when 'event_callback'
-      # Verify requests
-      return if request_data['token'] != ENV['SLACK_VERIFICATION_TOKEN']
+  # post '/slack/events' do
+  #   request_data = JSON.parse(request.body.read)
+  #   case request_data['type']
+  #   when 'url_verification'
+  #     # URL Verification event with challenge parameter
+  #     request_data['challenge']
+  #   when 'event_callback'
+  #     # Verify requests
+  #     return if request_data['token'] != ENV['SLACK_VERIFICATION_TOKEN']
 
-      event_data = request_data['event']
-      case event_data['type']
-        # Message IM event
-      when 'message'
-        return if event_data['subtype'] == 'bot_message' || event_data['subtype'] == 'message_changed'
+  #     event_data = request_data['event']
+  #     case event_data['type']
+  #       # Message IM event
+  #     when 'message'
+  #       return if event_data['subtype'] == 'bot_message' || event_data['subtype'] == 'message_changed'
 
-        Bot.handle_direct_message(event_data) if event_data['user']
-      else
-        puts "Unexpected events\n"
-      end
-      status 200
-    end
-  end
+  #       Bot.handle_direct_message(event_data) if event_data['user']
+  #     else
+  #       puts "Unexpected events\n"
+  #     end
+  #     status 200
+  #   end
+  # end
+
+  use SlackAuthorizer
 
   post '/slack/command' do
     'OK'

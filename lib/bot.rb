0c7d9c4855207c965ea0b0ae29914b2b8729b27d
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-08-21 16:35:03 +0300

    Arrange turn base

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 require 'slack-ruby-client'
 require_relative './stack_fetcher.rb'
 require_relative './post_slack.rb'
 require 'pry'
 
 class Bot # rubocop:todo Metrics/ClassLength
-
   def self.new_game(question) # rubocop:todo Metrics/MethodLength
     [{
       color: '#5DFF00',
@@ -49,34 +50,6 @@ class Bot # rubocop:todo Metrics/ClassLength
     }]
   end
 
-  # def self.new_game(question) # rubocop:todo Metrics/MethodLength
-  #   [{
-  #     color: '#5DFF00',
-  #     title: question,
-  #     callback_id: 'start:search',
-  #     actions: [
-  #       {
-  #         name: 'start',
-  #         text: '   YES   ',
-  #         type: 'button',
-  #         value: 'start:search'
-  #       }
-  #     ]
-  #   }, {
-  #     color: '#FF0000',
-  #     title: '',
-  #     callback_id: 'finish:search',
-  #     actions: [
-  #       {
-  #         name: 'start',
-  #         text: '   NO   ',
-  #         type: 'button',
-  #         value: 'finish:search'
-  #       }
-  #     ]
-  #   }]
-  # end
-
   def self.link_chooser(object)
     array = []
     object.each_with_index do |_item, index|
@@ -116,13 +89,13 @@ class Bot # rubocop:todo Metrics/ClassLength
     }
   end
 
-  def self.link_creator(user_id)
+  def self.link_creator(_user_id)
     stack_variable = fetch_stack('asc', 'ruby', 'none')
     stack_links = link_chooser(stack_variable['items'])
     attachment = []
     attachment << {
       color: '#FFA500',
-      callback_id: 'post:post',
+      callback_id: 'finish:search',
       title: 'Goodbye!',
       actions: [
         {
@@ -173,13 +146,14 @@ class Bot # rubocop:todo Metrics/ClassLength
     plays(user_id)
     if @plays[user_id].nil?
       intro(user_id)
-
+      @plays[user_id] = 'User'
     else
       client = Slack::Web::Client.new
       client.chat_postMessage(
         channel: msg['channel'],
-        text: 'Let\'s keep playing the last game'
+        text: 'Let\'s keep asking'
       )
+      @plays[user_id] = nil
     end
   end
 end
